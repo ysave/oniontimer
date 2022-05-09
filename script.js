770e789d3fc7c31b8d2decdf956743bbdef3161c
save <nick.c@menisch.ch>
2022-05-09 22:42:59 +0200

    timer working

--- script.js
+++ script.js
@@ -8,20 +8,15 @@ function timer(){
 }
 
 function start(){
-    // Change the minutes and seconds to starting time
     minutes = 24;
     seconds = 59;
 
-    // Add the seconds and minutes to the page
     document.getElementById("minutes").innerHTML = minutes;
     document.getElementById("seconds").innerHTML = seconds;
 
-    // Start the countdown
-    var minutes_interval = setInterval(minutesTimer, 60000);
-    var seconds_interval = setInterval(secondsTimer, 1000);
+    var minutesInterval = setInterval(minutesTimer, 60000);
+    var secondsInterval = setInterval(secondsTimer, 1000);
 
-    // Functions
-    // Function for minute counter
     function minutesTimer() {
         minutes = minutes - 1;
         document.getElementById("minutes").innerHTML = minutes;
@@ -37,25 +32,11 @@ function start(){
             document.getElementById("seconds").innerHTML = seconds;
         }
 
-        // Check if the seconds and minutes counter has reached 0
-        // If reached 0 then end the session
         if (seconds <= 0) {
             if (minutes <= 0) {
-                // Clears the interval i.e. stops the counter
-                clearInterval(minutes_interval);
-                clearInterval(seconds_interval);
-
-                // Add the message to the html
-                document.getElementById("done").innerHTML =
-                    "Session Completed!! Take a Break";
-
-                // Make the html message div visible
-                document.getElementById("done").classList.add("show_message");
-
-                // PLay the bell sound to tell the end of session
-                //bell.play();
+                clearInterval(minutesInterval);
+                clearInterval(secondsInterval);
             }
-            // Reset the session seconds to 60
             seconds = 60;
         }
     }
